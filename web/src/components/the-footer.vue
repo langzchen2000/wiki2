455728ff0b76898edb8dc4ddf123149356ae522d
langs <630250445@qq.com>
2023-05-31 22:51:43 +0800

    4-7 the footer, the header

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,10 @@
+<a-layout-footer style="text-align: center">
+jiawa.wiki2
+</a-layout-footer>
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    name: 'the-footer'
+})
+</script>
